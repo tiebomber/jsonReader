6296176e314f3c67e055d81e9a565de82066904e
tiebomber <tiebomber7512@gmail.com>
2019-10-14 23:22:18 +0700

    Update version org.apache.commons:commons-compress

--- src/main/java/jsonReader.java
+++ src/main/java/jsonReader.java
@@ -1,5 +1,7 @@
 import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorInputStream;
@@ -8,7 +10,6 @@ import org.apache.commons.compress.utils.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,15 +29,17 @@ public class jsonReader {
     private static final String JSON_KEY = "uids";
 
     public static void main(String[] args) {
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.print("Input archive full name: ");
             String archiveName = reader.readLine();
-            List<String> jsonList = getJSONListFromArchive(archiveName);
+            List<String> jsonList = getJSONs(archiveName);
             mergeUsers(jsonList);
-            System.out.println("User ID: " + maxGroupsUser(groupCounts));
-        } catch (CompressorException | IOException e) {
+            System.out.println(maxGroupsUser(groupCounts));
+        } catch (IOException| ArchiveException | CompressorException | NullPointerException | IllegalArgumentException e) {
             e.printStackTrace();
         }
+
     }
 
     public static void mergeUsers(List<String> jsonList) {
@@ -56,7 +59,7 @@ public class jsonReader {
         }
     }
 
-    public static  <K, V extends Comparable<V>> K maxGroupsUser(Map<K, V> map) {
+    public static <K, V extends Comparable<V>> K maxGroupsUser(Map<K, V> map) {
         Optional<Map.Entry<K, V>> maxEntry = map.entrySet()
                 .stream()
                 .max(Comparator.comparing(Map.Entry::getValue)
@@ -64,35 +67,75 @@ public class jsonReader {
         return maxEntry.get().getKey();
     }
 
-    public static List<String> getJSONListFromArchive(String archiveName) throws IOException, CompressorException {
+    public static List<String> getJSONs(String archiveName) throws IOException, CompressorException, ArchiveException, IllegalArgumentException, NullPointerException {
 
-        if (archiveName == null || archiveName.isEmpty())
-            throw new FileNotFoundException("Archive name is null or empty");
+        if (archiveName == null)
+            throw new NullPointerException("Archive name is null.");
+
+        if (archiveName.isEmpty())
+            throw new IllegalArgumentException("Archive name is empty.");
+
+        try (InputStream fileInputStream = new BufferedInputStream(new FileInputStream(archiveName))) {
+            if (isSupportedArchiveFormat(archiveName))
+                return getJSONsFromArchive(fileInputStream);
+            else
+                return getJSONsFromCompressor(fileInputStream);
+        }
+    }
+
+    public static boolean isSupportedArchiveFormat(String archiveName) {
+        String archiveFormat = archiveName.substring(archiveName.lastIndexOf('.') + 1);
+        switch (archiveFormat) {
+            case "ar":
+            case "arj":
+            case "zip":
+            case "tar":
+            case "jar":
+            case "cpio":
+            case "dump":
+            case "7z":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static List<String> getJSONsFromArchive(InputStream fileInputStream) throws IOException, ArchiveException {
+
+        try(ArchiveInputStream archiveInputStream = new ArchiveStreamFactory().createArchiveInputStream(fileInputStream)) {
+            return getJSONsList(archiveInputStream);
+        }
+    }
+
+    private static List<String> getJSONsFromCompressor(InputStream fileInputStream) throws IOException, CompressorException {
+
+        try(CompressorInputStream compressorInputStream = new CompressorStreamFactory().createCompressorInputStream(fileInputStream);
+            ArchiveInputStream archiveInputStream = new TarArchiveInputStream(compressorInputStream)){
+            return getJSONsList(archiveInputStream);
+        }
+
+    }
+
+    private static List<String> getJSONsList(ArchiveInputStream archiveInputStream) throws IOException {
 
         List<String> result = new ArrayList<>();
-        try (InputStream inputStream = new BufferedInputStream(new FileInputStream(archiveName));
-             CompressorInputStream compressorInputStream = new CompressorStreamFactory().createCompressorInputStream(inputStream);
-             ArchiveInputStream archiveInputStream = new TarArchiveInputStream(compressorInputStream)) {
+        ArchiveEntry entry;
 
-            ArchiveEntry entry;
-
-            while ((entry = archiveInputStream.getNextEntry()) != null) {
-                if (!archiveInputStream.canReadEntryData(entry)) {
-                    continue;
-                }
-                if (!entry.isDirectory() && isJSONFile(entry.getName())) {
-                    byte[] buf = new byte[(int) entry.getSize()];
-                    IOUtils.readFully(archiveInputStream, buf);
-                    result.add(new String(buf));
-                }
+        while ((entry = archiveInputStream.getNextEntry()) != null) {
+            if (!archiveInputStream.canReadEntryData(entry)) {
+                continue;
             }
-
+            if (!entry.isDirectory() && isJSONFile(entry.getName())) {
+                byte[] buf = new byte[(int) entry.getSize()];
+                IOUtils.readFully(archiveInputStream, buf);
+                result.add(new String(buf));
+            }
         }
         return result;
     }
 
     private static boolean isJSONFile(String fileName) {
-        return fileName.matches(".+\\.json$");
+        return fileName.toLowerCase().matches(".+\\.json$");
     }
 
 }
